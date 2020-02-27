89462cabd60fc497f0d1c691b6231ebceae13408
youp bos <2107418@talnet.nl>
2020-02-27 10:56:16 +0100

    Uitwerking van de exercise

---
+++ error_log.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Skateboard <small>(#1)</small></h1>
+    <h1>Basketbal <small>(#2)</small></h1>
+    <h1>Skeelers <small>(#3)</small></h1>
+
+    <form action="error_log.php" method="_GET">
+        <input type="number" name="input" id="">
+        <button type="submit">submit</button>
+    </form>
+    <?php
+    try {
+        if (($_GET["input"] > 0) && ($_GET["input"] < 4)) {
+            $_SESSION["choice"] = $_GET["input"];
+            echo "<h1>Gekozen item: (#" . $_SESSION["choice"] . ")</h1>";
+        } else {
+            throw new Exception("geen geldige hoeveelheid");
+        }
+    } catch (Exception $e) {
+        error_log("Gebruiker heeft een verkeet getal op gegeven!", 3, "error.php");
+        echo "<h3>'fout melding gevonden': " . $e->getMessage(), "\n" . "</h3>";
+    }
+    ?>
+</body>
+
+</html>
